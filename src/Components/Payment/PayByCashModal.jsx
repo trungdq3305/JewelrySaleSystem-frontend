751940c3f7b9d4e270513f1e01e7854fedf5bbe7
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2024-07-10 09:56:28 +0700

    fix axios

--- src/Components/Payment/PayByCashModal.jsx
+++ src/Components/Payment/PayByCashModal.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const PayByCashModal = () => {
-  return <div>PayByCashModal</div>
+  return <>
+  
+  </>
 }
 
 export default PayByCashModal
--- src/Configs/axios.js
+++ src/Configs/axios.js
@@ -45,21 +45,21 @@ export const getAllProductsv2 = async (
   }
 }
 
-export const getAllGem = async () => {
-  try {
-    const data = await axios.get(api + '/gem')
-    console.log(data.data)
-    return data.data
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.log('error massage: ', error.message)
-      return error.message
-    } else {
-      console.log('Unexpected error: ', error)
-      return 'An unexpected error has occired'
-    }
-  }
-}
+// export const getAllGem = async () => {
+//   try {
+//     const data = await axios.get(api + '/gem')
+//     console.log(data.data)
+//     return data.data
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       console.log('error massage: ', error.message)
+//       return error.message
+//     } else {
+//       console.log('Unexpected error: ', error)
+//       return 'An unexpected error has occired'
+//     }
+//   }
+// }
 export const addProduct = async (formData) => {
   try {
     const data = await axios.post(api + '/products/create-product', formData)
